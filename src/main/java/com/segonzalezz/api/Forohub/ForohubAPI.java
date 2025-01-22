178de72b4377f5fc2778c83ee2ca5890f49d15d5
DaironAVR <daironpvargas@gmail.com>
2025-01-21 21:34:54 -0500

    Cambios1

---
+++ src/main/java/com/segonzalezz/api/Forohub/ForohubAPI.java
@@ -0,0 +1,13 @@
+package com.segonzalezz.api.Forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubAPI.class, args);
+	}
+
+}
